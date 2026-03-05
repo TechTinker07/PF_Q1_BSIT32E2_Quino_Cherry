1a6918c117ae2f78ae5e791435e1aa6dc1538d45
Cherry Quino <cherryqui25@gmail.com>
2026-03-06 02:19:20 +0800

    Add PokemonSelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import PokemonCard from './components/PokemonCard';
+import PokemonSelector from './components/PokemonSelector';
 import type { Pokemon } from './types';
 
-interface AppProps {
-  pokemonData: Pokemon;
-}
+export default function App() {
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
-export default function App({ pokemonData }: AppProps) {
+  function handleSelect(name: string) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then(res => res.json())
+      .then(data => {
+        const clean: Pokemon = {
+          id: data.id,
+          name: data.name,
+          height: data.height,
+          weight: data.weight,
+          imageUrl: data.sprites.front_default
+        };
+        setPokemon(clean);
+      });
+  }
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
-      <h1>PokéAPI Prop Demo</h1>
-      <PokemonCard pokemon={pokemonData} />
+      <h1>Pokémon Explorer</h1>
+      <PokemonSelector onSelect={handleSelect} />
+      {pokemon && <PokemonCard pokemon={pokemon} />}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import type { Pokemon } from './types'
 
-fetch('https://pokeapi.co/api/v2/pokemon/charizard')
-  .then(response => response.json())
-  .then(data => {
-    const cleanPokemon: Pokemon = {
-      id: data.id,
-      name: data.name,
-      height: data.height,
-      weight: data.weight,
-      imageUrl: data.sprites.front_default
-    }
-
-    createRoot(document.getElementById('root')!).render(
-      <StrictMode>
-        <App pokemonData={cleanPokemon} />
-      </StrictMode>
-    )
-  })
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
